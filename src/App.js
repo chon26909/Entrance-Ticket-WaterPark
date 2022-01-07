bb0872c61299ab7957c4131b686d41c16c83a1f5
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-07 23:24:49 +0700

    create routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+//components
+import Fallback from './components/Fallback/Fallback';
+import AdminLayout from './layouts/AdminLayout';
+import ProtectedRoute from "./auth/ProtectedRoute"
+
+//pages
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
+const Login = React.lazy(() => import('./pages/LoginPage'))
+
 function App() {
+
+  const activeSidebar = true;
+
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <React.Suspense fallback={<Fallback />}>
+        <Routes>
+          <Route path='/login' element={ <Login/> } />
+        </Routes>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path='/' element={ <Navigate to='/dashboard' /> }/>
+            <Route path='/dashboard' element={<AdminLayout />}>
+              <Route index element={<Dashboard />} />
+            </Route>
+          </Route>
+        </Routes>
+      </React.Suspense>
+    </BrowserRouter>
   );
 }
 
