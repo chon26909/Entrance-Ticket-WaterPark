30bd5590048803d30e3a6f174f94aa625946eb76
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-07 23:26:59 +0700

    default layout

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DefaultLayout = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path='/'>
+                    {/* <Route index element={<Dashboard />} /> */}
+                </Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default DefaultLayout
