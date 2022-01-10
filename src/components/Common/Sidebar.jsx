0ba4c603df94f194a8600f597a89b6f3e620d086
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-10 22:41:18 +0700

    add list menu item

--- src/components/Common/Sidebar.jsx
+++ src/components/Common/Sidebar.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import { Navigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css'
 
+const navItem = [
+  { name: 'Dashboard', path: 'dashboard' },
+  { name: 'Member', path: 'member' },
+  { name: 'Ticket', path: 'ticket' }
+]
+
 const Sidebar = ({activeSidebar}) => {
-    return (
-        <div>
-        <div id="sidebar" className={activeSidebar ? 'active' : ''}>
-          <div className="logo">
-            <Navigate to='/dashboard'>
-              <h5>Water Park</h5>
-            </Navigate>
-          </div>
-          <div className="nav">
-            <Navigate to='/dashboard'>
-              <h5>Dashboard</h5>
-            </Navigate>
-          </div>
+  return (
+    <div>
+      <div id="sidebar" className={activeSidebar ? 'active' : 'hidden'}>
+        <div className="logo">
+          <NavLink to='dashboard'>
+            Water Park
+          </NavLink>
         </div>
+        {
+          navItem.map((item) => {
+            return <div className='nav' key={item.name}>
+              <NavLink to={item.path} className={({ isActive }) => isActive ? 'link-active' : ''} >
+                {item.name}
+              </NavLink>
+            </div>
+          })
+        }
+
       </div>
-    )
+    </div>
+  )
 }
 
 export default Sidebar;
