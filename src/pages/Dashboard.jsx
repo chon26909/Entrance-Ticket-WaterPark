dd0e54b920c964e693a1fc791e1d77e2da5eb4fb
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-11 22:37:18 +0700

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import SellTicket from '../components/Widget/SellTicket'
+import SellTicketChart from '../components/Widget/SellTicketChart'
 
 const Dashboard = () => {
 
     return (
         <div>
           <h2>Dashboard</h2>
-          <SellTicket/>
+          <SellTicketChart/>
         </div>
     )
 }
