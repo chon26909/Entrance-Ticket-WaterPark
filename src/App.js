fbd570f758bd8aa9ceba3e6f752da137bca974b0
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-11 22:36:01 +0700

    update routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+//routes
+import routes from './routes';
 
 //components
 import Fallback from './components/Fallback/Fallback';
 import AdminLayout from './layouts/AdminLayout';
 import ProtectedRoute from "./auth/ProtectedRoute"
 
-//pages
 const Login = React.lazy(() => import('./pages/LoginPage'));
 
-const Dashboard = React.lazy(() => import('./pages/Dashboard'));
-const Member = React.lazy(() => import('./pages/MembersPage'));
-const Ticket = React.lazy(() => import('./pages/TicketPage'));
-
-const routes = [
-  { path: 'dashboard', element: Dashboard },
-  { path: 'member', element: Member },
-  { path: 'ticket', element: Ticket }
-]
-
 function App() {
-
-  const activeSidebar = true;
 
   return (
     <BrowserRouter>
@@ -37,8 +26,8 @@ function App() {
 
             <Route element={<AdminLayout />}>
               {
-                routes.map((route) => {
-                  return <Route key={route.path} path={route.path} element={< route.element />} />
+                routes.map((route, key) => {
+                  return <Route key={key} path={route.path} element={< route.element />} />
                 })
               }
             </Route>
