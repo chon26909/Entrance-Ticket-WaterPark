a4f5f827dd33087fe43e35142319ed97f0a740ae
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-11 22:38:38 +0700

    update ticket page

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import { Button, Grid } from 'semantic-ui-react'
+
+import AdminLayout from '../layouts/AdminLayout';
+
+import SellTicketPage from './SellTicketPage';
+
 
 const TicketPage = () => {
     return (
         <div>
-            ticket
+            <h1>จัดการบัตร</h1>
+            <Grid columns={2} >
+                <Grid.Column>
+                    <Link to='create' >
+                        <Button color='twitter' size='big'>
+                            ขายบัตร
+                        </Button>
+                    </Link>
+                </Grid.Column>
+
+            </Grid>
+
         </div>
     )
 }
