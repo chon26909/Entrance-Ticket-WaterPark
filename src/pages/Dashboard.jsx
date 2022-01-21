54cc41c4f337c714213ebe042530805a2f9718c0
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-21 23:30:46 +0700

    change dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SellTicketChart from '../components/Widget/SellTicketChart'
+import SummaryDashBoard from '../components/Common/SummaryDashBoard';
 
 const Dashboard = () => {
 
     return (
         <div>
           <h2>Dashboard</h2>
+          <SummaryDashBoard/>
           <SellTicketChart/>
         </div>
     )
