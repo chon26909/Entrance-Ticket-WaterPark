eea8e6cc47f45d7cc40b123f4dba6c6f9204aae3
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-01-21 23:29:37 +0700

    add content

---
+++ src/components/Common/SummaryDashBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardSummary from '../Widget/CardSummary';
+
+const SummaryDashBoard = () => {
+    return (
+        <div>
+            <CardSummary color='red'/>
+        </div>
+    )
+}
+
+export default SummaryDashBoard;
